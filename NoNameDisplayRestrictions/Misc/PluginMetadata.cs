ad1a54faf070a6660522c25ebd2c45c665212ee9
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2025-11-22 01:29:05 +0000

    Update PluginMetadata.cs

--- NoNameDisplayRestrictions/Misc/PluginMetadata.cs
+++ NoNameDisplayRestrictions/Misc/PluginMetadata.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.github.WhiteSpike.NoNameDisplayRestrictions";
         public const string NAME = "No Name Display Restrictions";
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.0.1";
     }
 }
