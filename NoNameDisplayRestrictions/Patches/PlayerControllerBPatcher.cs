73edc4b341cd6b9f28e16e927531dac2b2a301fc
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2025-11-24 14:52:51 +0000

    Fixed IL error due to forgetting to delete the ``this`` argument used for method calls

--- NoNameDisplayRestrictions/Patches/PlayerControllerBPatcher.cs
+++ NoNameDisplayRestrictions/Patches/PlayerControllerBPatcher.cs
@@ -17,15 +17,26 @@ namespace NoNameDisplayRestrictions.Patches
 		static IEnumerable<CodeInstruction> SendNewPlayerValuesClientRpcTranspiler(IEnumerable<CodeInstruction> instructions)
 		{
 			MethodInfo NoPunctuation = typeof(PlayerControllerB).GetMethod(nameof(PlayerControllerB.NoPunctuation), BindingFlags.NonPublic | BindingFlags.Instance);
-			ConstructorInfo stringConstructor = typeof(string).GetConstructor([typeof(char[])]);
+			ConstructorInfo stringConstructor = typeof(string).GetConstructor([typeof(ReadOnlySpan<char>)]);
+			MethodInfo op_Implicit = typeof(string).GetMethod("op_Implicit", [typeof(string)]);
 			List<CodeInstruction> codes = new(instructions);
 			int index = 0;
 			Tools.FindMethod(ref index, ref codes, NoPunctuation, skip: true, errorMessage: "Couldn't find the NoPunctuation method callback which removes the non-letter characters from the player's name");
-			codes.RemoveAt(index-1);
+			codes.RemoveAt(index - 1);
 			codes.Insert(index - 1, new CodeInstruction(opcode: OpCodes.Newobj, stringConstructor));
-			int ldlockIndex = index;
+			codes.Insert(index - 1, new CodeInstruction(opcode: OpCodes.Call, op_Implicit));
+			for (; index > 0; index--)
+			{
+				if (codes[index].opcode == OpCodes.Ldarg_0)
+				{
+					codes.RemoveAt(index);
+					Tools.FindLocalField(ref index, ref codes, localIndex: 0, store: false, skip: true);
+					break;
+				}
+			}
+			int ldlockIndex = index - 1;
 			Tools.FindLocalField(ref index, ref codes, localIndex: 0, store: true, skip: true, errorMessage: "Couldn't find the store instruction which removes characters from the player's name");
-			codes.RemoveRange(ldlockIndex, index - ldlockIndex - 1);
+			codes.RemoveRange(ldlockIndex, index - ldlockIndex);
 			return codes;
 		}
 	}
